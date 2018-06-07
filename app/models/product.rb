861ec2ebcc8b75da4b9dbaf6e30ec2e40e85f95c
Raghu Betina <raghu@firstdraft.com>
2018-06-07 20:15:09 +0000

    Direct association between products and reviews

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ApplicationRecord
   # Direct associations
 
+  has_many   :reviews,
+             :foreign_key => "product_number",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,10 @@
 class Review < ApplicationRecord
   # Direct associations
 
+  belongs_to :product,
+             :foreign_key => "product_number",
+             :counter_cache => true
+
   # Indirect associations
 
   # Validations
